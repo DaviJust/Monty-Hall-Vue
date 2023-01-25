f8d7a9166395da0e7d34a8412be8b55035ae7218
Davi <davijustinocouto@gmail.com>
2023-01-25 19:32:41 -0300

    Titulo e estilo da pagina

---
+++ montyhall/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monty Hall</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+*{
+   
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+}
+body{
+    background: linear-gradient(to right, #1a2980, #26d0ce); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    color:white;
+}
+#app{
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+}
+#app h1{
+    border: 1px solid black;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom:60px;
+}
+</style>
