b878a88120baad1c3af82e92d9269f6925531d84
Davi <davijustinocouto@gmail.com>
2023-01-25 21:16:25 -0300

    Door and GiftDoor

---
+++ montyhall/src/components/DoorMonty.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="door-area">
+    <div class="door-frame">
+        <Gift/>
+    </div>
+    <div class="door">
+        <div class="number">{{ number }}</div>
+        <div class="knob"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GiftDoor from './GiftDoor.vue'
+
+export default {
+name: 'DoorMonty',
+components: {GiftDoor},
+props:{
+    number:{},
+    hasGift: {type: Boolean}
+},
+data:function(){
+    return {
+        open: false,
+        selected: false
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
